27977ffb80148129ee65a3c7ad6cdd09a73a6292
inluo <93850531+inluo@users.noreply.github.com>
2021-12-03 17:18:30 +0800

    Update UserController.java

--- localGitPro/src/com/nnxy/controller/UserController.java
+++ localGitPro/src/com/nnxy/controller/UserController.java
@@ -1,5 +1,12 @@
 package com.nnxy.controller;
 
 public class UserController {
+  [branch "master"] 
+	remote = origin 
+	merge = refs/heads/master
+  [remote "origin"] 
+	url = git@github.com:inluo/gitProject.git
+	fetch = +refs/heads/*:refs/remotes/origin/* 
+	push = refs/heads/master:refs/heads/master
 
 }
